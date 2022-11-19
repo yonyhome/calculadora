0da3e91b7027ae7b56d8c5f07ddcf91f09d0effb
yonyhome <yonyh@uninorte.edu.co>
2022-11-18 22:16:12 -0500

    calculadora

---
+++ src/calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+
+package calculadora;
+
+import views.NewJFrame;
+
+/**
+ *
+ * @author yonyh
+ */
+public class Calculadora {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        new NewJFrame().setVisible(true);
+    }
+    
+}
